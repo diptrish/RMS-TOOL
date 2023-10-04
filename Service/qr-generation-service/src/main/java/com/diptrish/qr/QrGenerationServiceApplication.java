89ee7f13ad1dc33792018f3c1315e3755f021859
Dipankar <dipankarmalick97@gmail.com>
2023-10-04 15:50:42 +0530

    first Commit

---
+++ Service/qr-generation-service/src/main/java/com/diptrish/qr/QrGenerationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.diptrish.qr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrGenerationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrGenerationServiceApplication.class, args);
+	}
+
+}
---
+++ Service/qr-generation-service/src/test/java/com/diptrish/qr/QrGenerationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diptrish.qr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrGenerationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
